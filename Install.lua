8c551488b46caa99a1f87a857ee43a74a23694ff
liloulap <168288666+liloulap@users.noreply.github.com>
2024-04-29 18:24:50 +0200

    Update Install.lua

--- Install.lua
+++ Install.lua
@@ -5,5 +5,14 @@ print("Instalation en cours")
 print("--------------------")
 
 run("wget https://raw.githubusercontent.com/liloulap/BibiOS/main/.BibiOS/boot.lua")
+
+run("clear")
+
+print("--------------------")
+print("Instalation en cours")
+print("--------------------")
+
+os.sleep(3)
+
 run("cd .BibiOS")
 run("boot.lua")
